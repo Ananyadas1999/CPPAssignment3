37086e4cdc615dada15c28309038ee6fa3e2cc32
Ananyadas1999 <31275682+Ananyadas1999@users.noreply.github.com>
2017-09-15 21:39:18 +0530

    Update CPP16.cpp

--- CPP16.cpp
+++ CPP16.cpp
@@ -7,7 +7,7 @@ int main()
 {
   float s1,s2,s3;
   cout<<"Program to check whether the triangle is equilateral, isosceles or scalene triangle.";
-  cout<<"\n \nEnter the three angles of the triangle: ";
+  cout<<"\n \nEnter the three sides of the triangle: ";
   cin>>s1>>s2>>s3;
   if(((s1+s2)>s3)&&((s2+s3)>s1)&&((s3+s1)>s2))
   { 
